30bfaeb88aaf162822fd3d771a9900761ff3a770
kaluaim <kaluaim@gmail.com>
2017-02-26 19:49:34 -0500

    added results for US01 and US07

--- theproject/__main__.py
+++ theproject/__main__.py
@@ -1,5 +1,5 @@
 import gedcom
-import US0408sourabh
+import US0408sourabh as US0408
 import util_khalid
 
 FILE_PATH = 'GEDCOM1.ged'
@@ -12,16 +12,15 @@ if __name__ == '__main__':
     gedcom.print_families()
 
     # US01
-    print('--- US01: Dates before current date ---')
+    print('\n--- US01: Dates before current date ---')
     fam_check = util_khalid.check_family_dates_before_current(gedcom.FAMs)
     indi_check = util_khalid.check_individual_dates_before_current(gedcom.INDIs)
 
     # US07
     print('\n--- US07: Less then 150 years old ---')
     util_khalid.check_individual_age_less_than_150(gedcom.INDIs)
-    
+
     #US04
     US0408.US4MbD()
     #US08
-    US0408.US4bbmp()
-    
+    US0408.US08bbmp()
