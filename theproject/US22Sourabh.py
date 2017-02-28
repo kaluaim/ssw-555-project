0f55746854f17f16a5a8a361e3dbda207df3cbff
sourabh kulkarni <skulkar6@stevens.edu>
2017-02-27 22:27:48 -0500

    Create US22Sourabh.py

---
+++ theproject/US22Sourabh.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+import gedcom
+
+def US22UniqueIDs():
+    print("User Story 22")
+    x=0
+    y=1
+    
+    for k,i in gedcom.FAMs.items():
+        if i.famid[x]==i.famid[y]:
+           print("IDs are similar")
+           x=x+1;
+           y=y+1;
+        else:
+            print("Family id Not Similar")
+
+    for k,i, in gedcom.INDIs.items():
+        if i.indiid[x]==i.indiid[y]:
+           print("IDs are similar")
+           x=x+1;
+           y=y+1;
+        else:
+            print("individual id Not Similar")
