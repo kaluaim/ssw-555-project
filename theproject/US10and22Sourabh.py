df56ac91f89bcd0702b6e2900d78b17b63a7389b
sourabh kulkarni <skulkar6@stevens.edu>
2017-03-20 18:15:26 -0400

    Update US10and22Sourabh.py

--- theproject/US10and22Sourabh.py
+++ theproject/US10and22Sourabh.py
@@ -1,19 +1,35 @@
 from datetime import datetime, timedelta
 import gedcom
 
+from datetime import datetime, timedelta
+import gedcom
+# check Fam IDs
 def US22UniqueIDs():
     x=0
-    y=1
+    y=x+1
+
     for k,i in gedcom.FAMs.items():
-        if i.famid[x]==i.famid[y]:
-           print('ERROR: FAMILY: US22: ' + i.famid[x] + ' IDs are similar')
-           x=x+1;
-           y=y+1;
-    for k,i, in gedcom.INDIs.items():
-        if i.indiid[x]==i.indiid[y]:
-           print('ERROR: INDIVIDUAL: US22: ' + i.indiid[x] + ' IDs are similar')
-           x=x+1;
-           y=y+1;
+        if i.famid[x]!=i.famid[y]:
+            print();
+        else:
+            print('ERROR: FAMILY: US22: ' + i.famid + ' IDs are similar')
+            break;     
+        x=x+1;
+        
+    y=y+1;
+#check indi IDs
+def US22UniqueIDs1():
+    m=0;
+    n=m+1;
+    for k,i in gedcom.INDIs.items():
+        if i.indiid[m]!=i.indiid[n]:
+            print();
+        else:
+            print('ERROR: FAMILY: US22: ' + i.indiid + ' IDs are similar')
+            break;     
+        m=m+1;
+        
+    n=n+1;
 
 def US10Ma14():
     for l,m in gedcom.FAMs.items():
