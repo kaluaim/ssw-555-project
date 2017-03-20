8528d6bc2d3dd0b712a93375a5f76fc3d9a6db06
kaluaim <kaluaim@gmail.com>
2017-03-20 19:13:26 -0400

    updated US22

--- theproject/US10and22Sourabh.py
+++ theproject/US10and22Sourabh.py
@@ -4,31 +4,35 @@ import gedcom
 
 # check Fam IDs
 def US22UniqueIDs():
-    x=0
-    y=x+1
+    x = 0
+    fam_list = []
+    indi_list = []
 
-    for k,i in gedcom.FAMs.items():
-        if i.famid[x]!=i.famid[y]:
-            print();
-        else:
-            print('ERROR: FAMILY: US22: ' + i.famid + ' IDs are similar')
-            break;     
-        x=x+1;
-        
-    y=y+1;
-#check indi IDs
-def US22UniqueIDs1():
-    m=0;
-    n=m+1;
-    for k,i in gedcom.INDIs.items():
-        if i.indiid[m]!=i.indiid[n]:
-            print();
-        else:
-            print('ERROR: FAMILY: US22: ' + i.indiid + ' IDs are similar')
-            break;     
-        m=m+1;
-        
-    n=n+1;
+    for v in gedcom.FAMs.itervalues():
+        fam_list.append(v)
+
+    for v in gedcom.INDIs.itervalues():
+        indi_list.append(v)
+
+    while x < len(fam_list):
+        y = x + 1
+        while y < len(fam_list):
+            if fam_list[x].famid == fam_list[y].famid:
+                print('ERROR: FAMILY: US22: ' + fam_list[x].famid +
+                ' for husbandid ' + fam_list[x].husbandid +
+                ' IDs are similar to husbandid ' + fam_list[y].husbandid)
+            y = y + 1;
+        x = x + 1;
+
+        while x < len(indi_list):
+            y = x + 1
+            while y < len(indi_list):
+                if indi_list[x].indiid == indi_list[y].indiid:
+                    print('ERROR: INDIVIDUAL: US22: ' + indi_list[x].indiid +
+                    ' for ' + indi_list[x].name + ' IDs are similar to ' +
+                    indi_list[y].name)
+                y = y + 1;
+            x = x + 1;
 
 def US10Ma14():
     for l,m in gedcom.FAMs.items():
