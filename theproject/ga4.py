c7c1de1740d8e7641496df90ded6d0a766369089
kaluaim <kaluaim@gmail.com>
2017-04-04 18:56:33 -0400

    added test for US06

--- theproject/ga4.py
+++ theproject/ga4.py
@@ -6,11 +6,10 @@ def US06bbm():
     for k,i, in gedcom.INDIs.items():
         if i.death != 'N/A' and i.spouse != 'N/A':
             for l,m in gedcom.FAMs.items():
-                if i.spouse == m.famid :
-                    if m.divorced != 'N/A' and m.divorced != 'N/A':
-                        deaddate= datetime.strptime(i.death, '%d %b %Y')
-                        divdate= datetime.strptime(m.divorced, '%d %b %Y')
-                        if  deaddate < divdate:
-                            print("ERROR: FAMILY: US06: " + m.famid + ": Divorce "
-                                + m.divorced + " after " + i.indiid + " Death " +
-                                i.death)
+                if i.spouse == m.famid and m.divorced != 'N/A':
+                    deaddate= datetime.strptime(i.death, '%d %b %Y')
+                    divdate= datetime.strptime(m.divorced, '%d %b %Y')
+                    if  deaddate < divdate:
+                        print("ERROR: FAMILY: US06: " + m.famid + ": Divorce "
+                            + m.divorced + " after " + i.indiid + " Death " +
+                            i.death)
